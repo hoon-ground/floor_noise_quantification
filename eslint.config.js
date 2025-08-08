24dfa04e3052dd60862448ef148847ea57e9f706
hoon_ground <break_the_limit@naver.com>
2025-08-08 19:44:16 +0900

    chore : ESLint, prettier 적용

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  react.configs.recommended,
+  reactHooks.configs.recommended,
+  {
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
