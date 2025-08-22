13eea6f299af61d18b3705b6e6363b6d2c916297
hoon_ground <break_the_limit@naver.com>
2025-08-22 14:24:36 +0900

    chore : 기타 패키지 추가 및 수정

--- src/shared/ui/GlobalStyle.js
+++ src/shared/ui/GlobalStyle.js
@@ -6,14 +6,24 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    text-align:center;
   }
+
   html, body {
     height: 100%;
     font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
-    background-color: #fff;
+    background-color: #ebebebff;
     color: #000;
   }
+
+  #root{
+    width: 100%;
+    max-width: 720px;
+    background-color: #ffffff;
+    margin: 0 auto;
+  }
+
   a {
     text-decoration: none;
     color: inherit;
